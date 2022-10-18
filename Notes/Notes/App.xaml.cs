99f5812e52affb73c78adc09945e4d4f96696364
Mika Chang <83247653+mcsqr@users.noreply.github.com>
2022-10-18 00:37:31 -0500

    updated app to work with database

--- Notes/Notes/App.xaml.cs
+++ Notes/Notes/App.xaml.cs
@@ -1,13 +1,43 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////
+// Main File:		 App.xaml
+// This File:        App.xaml.cs
+//
+// Author:           Mika Chang
+// Email:            mikacchang@wisc.edu
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.IO;
+using Notes.Data;
 using Xamarin.Forms;
 
 namespace Notes {
+    /// <summary>
+    /// Class for the app object which instantiates the app.
+    /// </summary>
     public partial class App : Application {
-        public static string FolderPath { get; private set; }
+        static NoteDatabase database;
+
+        /// <summary>
+        /// Create the database connection as a singleton.
+        /// </summary>
+        public static NoteDatabase Database {
+            get {
+                if (database == null) {
+                    database = new NoteDatabase(Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        /// <summary>
+        /// Instantiate the app.
+        /// </summary>
         public App() {
             InitializeComponent();
-            FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
             MainPage = new AppShell();
         }
 
