97c5588283aea2201e06cd00969cb3150aae21e4
Mika Chang <83247653+mcsqr@users.noreply.github.com>
2022-10-18 00:53:31 -0500

    Updated NotesPage to work with the db

--- Notes/Notes/Views/NotesPage.xaml.cs
+++ Notes/Notes/Views/NotesPage.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿////////////////////////////////////////////////////////////////////////////////
+// Main File:		 App.xaml
+// This File:        NotesPage.xaml.cs
+//
+// Author:           Mika Chang
+// Email:            mikacchang@gmail.com
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,42 +15,43 @@ using Xamarin.Forms;
 
 namespace Notes.Views {
     public partial class NotesPage : ContentPage {
+
         public NotesPage() {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Method that defines behavior when the app appears, creating a data
+        /// source for the CollectionView
+        /// </summary>
         protected override void OnAppearing() {
             base.OnAppearing();
 
-            var notes = new List<Note>();
-
-            // Create a Note object from each file.
-            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
-            foreach (var filename in files) {
-                notes.Add(new Note {
-                    Filename = filename,
-                    Text = File.ReadAllText(filename),
-                    Date = File.GetCreationTime(filename)
-                });
-            }
-
-            // Set the data source for the CollectionView to a
-            // sorted collection of notes.
-            collectionView.ItemsSource = notes
-                .OrderBy(d => d.Date)
-                .ToList();
+            // Retrieve all notes from database and set as source for CollectionView.
+            collectionView.ItemsSource = await App.Database.GetNotesAsync();
         }
 
+        /// <summary>
+        /// Execute event handler when a button is pressed
+        /// </summary>
+        /// <param name="sender">the button that was pressed.</param>
+        /// <param name="e">the note.</param>
         async void OnAddClicked(object sender, EventArgs e) {
             // Navigate to the NoteEntryPage, without passing any data.
             await Shell.Current.GoToAsync(nameof(NoteEntryPage));
         }
 
+        /// <summary>
+        /// Go to the NoteEntryPage searching for the ID of the given Note
+        /// </summary>
+        /// <param name="sender">the button that was pressed.</param>
+        /// <param name="e">the note.</param>
         async void OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
             if (e.CurrentSelection != null) {
-                // Navigate to the NoteEntryPage, passing the filename as a query parameter.
+
+                // Navigate to the NoteEntryPage, passing the ID as a query parameter.
                 Note note = (Note)e.CurrentSelection.FirstOrDefault();
-                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.Filename}");
+                await Shell.Current.GoToAsync($"{nameof(NoteEntryPage)}?{nameof(NoteEntryPage.ItemId)}={note.ID.ToString()}");
             }
         }
     }
